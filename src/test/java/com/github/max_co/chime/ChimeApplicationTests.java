3b09a77be8a0d9f3b1c21cc04dd3f946018ff699
Massimo Comuzzo <43844835+max-co@users.noreply.github.com>
2025-03-23 20:58:39 +0100

    Add empty spring boot project

---
+++ src/test/java/com/github/max_co/chime/ChimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.max_co.chime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
