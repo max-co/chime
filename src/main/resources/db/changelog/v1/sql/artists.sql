cbad2615e07d29ae826f16621a0a758136b7b4c4
Massimo Comuzzo <43844835+max-co@users.noreply.github.com>
2025-04-05 16:27:56 +0200

    Add DB data migration

---
+++ src/main/resources/db/changelog/v1/sql/artists.sql
@@ -0,0 +1,1 @@
+INSERT INTO artist (id, name) VALUES (1, 'Marcus Tullius Cicero'), (2, 'Tully');
---
+++ src/main/resources/db/changelog/v1/sql/songs.sql
@@ -0,0 +1,40 @@
+INSERT INTO song (title, language, text, translation, artist_id) VALUES
+(
+    'short lorem ipsum', 'LA',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+sed do eiusmod tempor incididunt ut labore et dolore magnam aliquam quaerat voluptatem.
+Ut enim aeque doleamus animo, cum corpore dolemus, fieri tamen permagna accessio potest,
+si aliquod aeternum et infinitum impendere malum nobis opinemur.
+
+Quod idem licet transferre in voluptatem, ut postea variari voluptas distinguique possit,
+augeri amplificarique non possit. At.
+
+Quodsi Graeci leguntur a Graecis isdem de rebus alia ratione compositis, quid est,
+cur nostri a nostris de amicitia dicenda sunt. Praeclare enim Epicurus his paene verbis:
+\'Eadem\', inquit, scientia confirmavit animum, ne quod aut sempiternum aut diuturnum timeret malum.',
+    NULL,
+    2
+),
+(
+    'lorem ipsum', 'LA',
+    'Falsi putat. Confirmat autem illud vel maxime, quod ipsa natura divitias,
+quibus contenta sit, et parabilis et terminatas habet;
+inanium autem cupiditatum nec modus est ullus investigandi veri,
+nisi inveneris, et quaerendi defatigatio turpis est, cum esset.
+
+Ad alias litteras vocent, genus hoc scribendi, etsi sit elegans,
+personae tamen et dignitatis esse negent.
+Contra quos omnis dicendum breviter existimo. Quamquam philosophiae quidem vituperatoribus
+satis responsum est eo libro, quo a populo Romano locatus sum, debeo profecto.',
+
+    'quantumcumque possum, in eo non arbitrantur. Erunt etiam,
+et ii quidem eruditi Graecis litteris, contemnentes Latinas,
+qui se dicant in Graecis legendis operam malle consumere. Postremo.
+Percipit aut quid iudicat, quo aut petat aut fugiat aliquid,
+
+praeter voluptatem et dolorem. Ad haec et quae fugiamus refert omnia.
+Quod quamquam Aristippi est a Cyrenaicisque melius liberiusque defenditur,
+tamen eius modi tempora incidunt, ut labore et.
+Praeclare Scaevola: Graecum te, Albuci, quam Romanum atque Sabinum, municipem Ponti.',
+    1
+);
