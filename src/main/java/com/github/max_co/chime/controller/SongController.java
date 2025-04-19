2efdc61300c1af1eb669b42c2b116e434d79a0c2
Massimo Comuzzo <43844835+max-co@users.noreply.github.com>
2025-04-19 10:44:17 +0200

    Add song list view

---
+++ src/main/java/com/github/max_co/chime/controller/SongController.java
@@ -0,0 +1,41 @@
+// Copyright 2025 Massimo Comuzzo
+// SPDX-License-Identifier: Apache-2.0
+
+package com.github.max_co.chime.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.github.max_co.chime.dto.SongDto;
+import com.github.max_co.chime.service.SongService;
+
+@Controller
+@RequestMapping("song")
+public class SongController {
+
+  private SongService songService;
+
+  @Autowired
+  public SongController(SongService songService) {
+    this.songService = songService;
+  }
+
+  /**
+   * Shows page with song list.
+   *
+   * @param model
+   * @return
+   */
+  @GetMapping("")
+  public String showList(Model model) {
+    List<SongDto> songs = songService.findAll();
+    model.addAttribute("songs", songs);
+
+    return "song/list";
+  }
+}
