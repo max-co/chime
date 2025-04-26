2f8ee7afb7ce7b770219e992ae6a969bb002bf86
Massimo Comuzzo <43844835+max-co@users.noreply.github.com>
2025-04-26 22:02:53 +0200

    Add some SongService tests

---
+++ src/test/java/com/github/max_co/chime/service/DbSongServiceTest.java
@@ -0,0 +1,75 @@
+// Copyright 2025 Massimo Comuzzo
+// SPDX-License-Identifier: Apache-2.0
+
+package com.github.max_co.chime.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.github.max_co.chime.dto.ArtistDto;
+import com.github.max_co.chime.dto.SongDto;
+
+@SpringBootTest
+class DbSongServiceTest {
+
+  @Autowired
+  private DbSongService dbSongService;
+
+  @Autowired
+  private JdbcTemplate jdbc;
+
+  @BeforeEach
+  void setupDb() {
+    jdbc.execute("INSERT INTO artist (id, name) VALUES (1, 'Marcus Tullius Cicero'), (2, 'Tully');");
+    jdbc.execute("INSERT INTO song (title, language, text, translation, artist_id) VALUES " +
+        "('TITLE', 'LA', 'TEXT', 'TRANSLATION', 1), " +
+        "('Song', 'EN', 'La la la', NULL, NULL);");
+  }
+
+  @AfterEach
+  void cleanupDb() {
+    jdbc.execute("DELETE FROM song;");
+    jdbc.execute("DELETE FROM artist;");
+    jdbc.execute("ALTER TABLE song ALTER COLUMN id RESTART WITH 1");
+    jdbc.execute("ALTER TABLE artist ALTER COLUMN id RESTART WITH 1");
+  }
+
+  @Test
+  void testFindById() {
+    assertThrows(Exception.class, () -> dbSongService.findById(0));
+    SongDto dto = dbSongService.findById(1);
+    assertEquals(1, dto.getId());
+    assertEquals("TITLE", dto.getTitle());
+    assertEquals("LA", dto.getLanguage());
+    assertEquals("TEXT", dto.getText());
+    assertEquals("TRANSLATION", dto.getTranslation());
+    ArtistDto artist = dto.getArtist();
+    assertNotNull(artist);
+    assertEquals(1, artist.getId());
+    assertEquals("Marcus Tullius Cicero", artist.getName());
+    dto = dbSongService.findById(2);
+    assertEquals(2, dto.getId());
+    assertEquals("Song", dto.getTitle());
+    assertEquals("EN", dto.getLanguage());
+    assertEquals("La la la", dto.getText());
+    assertNull(dto.getTranslation());
+    assertNull(dto.getArtist());
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = { 1, 2 })
+  void testDeleteById(int id) {
+    SongDto dto = dbSongService.findById(id);
+    assertEquals(id, dto.getId());
+    dbSongService.deleteById(dto.getId());
+    assertThrows(Exception.class, () -> dbSongService.findById(id));
+  }
+}
